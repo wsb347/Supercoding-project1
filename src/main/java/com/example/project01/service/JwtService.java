c522324fb258f5878ef6a4a1fb7c28f49f3db190
장근영 <jky00914@naver.com>
2023-09-19 20:48:53 +0900

    hello

--- src/main/java/com/example/project01/service/JwtService.java
+++ src/main/java/com/example/project01/service/JwtService.java
@@ -4,12 +4,17 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.project01.controller.Dto.UserDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -33,18 +38,21 @@ public class JwtService {
     private void init(){
         algorithm = Algorithm.HMAC256(secretKey);
         jwtVerifier = JWT.require(algorithm).build();
+        byte[] accessSecret = secretKey.getBytes(StandardCharsets.UTF_8);
     }
 
     // Create JWT
-    public String encode(String password){
+    public String encode(UserDto userDto){
         LocalDateTime expiredAt = LocalDateTime.now().plusWeeks(4L);
         Date date = Timestamp.valueOf(expiredAt);
 
-        return JWT.create()
-                .withIssuer(issuer)
-                .withClaim(CLAIM_NAME_MEMBER_PASSWORD, password)
-                .withExpiresAt(date)
-                .sign(algorithm);
+        Claims claims = Jwts.claims().setSubject(userDto.getPassword());
+
+        return Jwts.builder().setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(date)
+                .signWith(algorithm, accessSecret)
+                .compact();
     }
 
     public Map<String, String> decode(String token){
