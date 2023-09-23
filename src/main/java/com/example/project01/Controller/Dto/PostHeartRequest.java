dc94bcce24cfe9f4e927af53c749a39002b50b39
wsb347 <wsb347@naver.com>
2023-09-24 06:41:03 +0900

    database 수정

---
+++ src/main/java/com/example/project01/Controller/Dto/PostHeartRequest.java
@@ -0,0 +1,20 @@
+package com.example.project01.controller.Dto;
+
+import com.example.project01.Entity.UserEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HeartRequest {
+
+    private long post_id;
+
+    private String user_id;
+}
