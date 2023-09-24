89b296570506b5f733615f2e4b04d32c7dda8932
wsb347 <wsb347@naver.com>
2023-09-24 18:02:12 +0900

    좋아요 기능 중복검사 추가

--- src/main/java/com/example/project01/Repository/HeartRepository.java
+++ src/main/java/com/example/project01/Repository/HeartRepository.java
@@ -2,6 +2,8 @@ package com.example.project01.repository;
 
 import com.example.project01.Entity.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,5 +11,9 @@ public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     Optional<Heart> findByUserId(String User);
 
-    Optional<Heart> findByUserIdAndPostId(String userId, long postId);
+//    Optional<Heart> findByUserIdAndPostId(String userId, long postId);
+
+    @Query("SELECT h FROM Heart h WHERE h.user.email = :userId AND h.postId = :postId")
+    Optional<Heart> findHeartByUserIdAndPostId(@Param("userId") String userId, @Param("postId") long postId);
+
 }
--- src/main/java/com/example/project01/Service/HeartService.java
+++ src/main/java/com/example/project01/Service/HeartService.java
@@ -27,12 +27,11 @@ public class HeartService {
 
     @Transactional
     public PostHeartRequest addHeart(HeartRequest heartRequest) {
+        System.out.println(userRepository.findByEmail(heartRequest.getUser_id()));
         Optional<UserEntity> optionalUserEntity = userRepository.findByEmail(heartRequest.getUser_id());
-        System.out.println("optionalUserEntity : " + optionalUserEntity);
         optionalUserEntity.orElseThrow(() -> new NotFoundException("유저를 찾을 수 없습니다."));
         Optional<Post> optionalPostEntity = postRepository.findById(heartRequest.getPost_id());
         Post post = optionalPostEntity.orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다."));
-
 
         if (userRepository == null) {
             log.error("userRepository NULL");
@@ -41,24 +40,29 @@ public class HeartService {
         if (heartRequest == null || heartRequest.getUser_id() == null) {
             log.error("heartRequest OR heartRequest NULL");
         }
-
-        // 이제 userEntity를 사용하여 Heart 객체 생성
+        System.out.println("heartRequest.getUser_id() : " + heartRequest.getUser_id());
+        System.out.println("heartRequest.getPost_id() : " + heartRequest.getPost_id());
+        var isHeart = heartRepository.findHeartByUserIdAndPostId(heartRequest.getUser_id(), heartRequest.getPost_id());
+        if(isHeart != null){
+            throw new RuntimeException("이미 좋아요를 눌렀습니다.");
+        }
         Heart heart = Heart.builder()
                 .user(optionalUserEntity.get())
                 .postId(optionalPostEntity.get().getId())
                 .build();
         heartRepository.save(heart);
 
-        PostHeartRequest postHeartResponse  = PostHeartRequest.builder()
+        PostHeartRequest postHeartResponse = PostHeartRequest.builder()
                 .post(post)
                 .heartRequest(heartRequest)
                 .build();
 
-        return postHeartResponse ;
+        return postHeartResponse;
+
     }
 
     public void removeHeart(Heart heart) throws IOException {
-        Optional<Heart> heartOpt = heartRepository.findByUserIdAndPostId(heart.getUser().getEmail(), heart.getPostId());
+        Optional<Heart> heartOpt = heartRepository.findHeartByUserIdAndPostId(heart.getUser().getEmail(), heart.getPostId());
         long deleteHeart = heartOpt.get().getId();
         heartRepository.deleteById(deleteHeart);
     }
